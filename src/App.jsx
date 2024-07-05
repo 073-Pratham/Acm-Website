b236b258f536085ae7650f9b47d6c99bffc31fc2
Pratham <prathamk7569@gmail.com>
2024-07-05 22:08:26 +0530

    removing chatButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
-import ChatButton from "./components/ChatButton";
-import { LoginProvider, useLogin } from "./components/contexts/LoginContext";
-import { useState } from "react";
-import { useEffect } from "react";
-import Login from "./pages/Login";
 
 function App() {
-
-  const {loginData} = useLogin();
   return (
     <>
     <Nav/>
     <Hero />
-    <ChatButton />
     </>
   );
 }
