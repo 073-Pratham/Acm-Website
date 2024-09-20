270202fe9e9b796489e05aabc1f23989f1d201ea
Pratham <prathamk7569@gmail.com>
2024-09-20 10:57:42 +0530

    Not found page

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import TeamPage from "./pages/TeamPage.jsx";
 import { LoginProvider } from "./components/contexts/LoginContext.jsx";
 import About from "./pages/About.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 const router = createBrowserRouter([
   {
@@ -53,6 +54,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
   },
+  {
+    path: "*",
+    element: <NotFound />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
